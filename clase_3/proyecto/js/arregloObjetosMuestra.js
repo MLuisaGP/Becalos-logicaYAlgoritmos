5a3c512d5eb97cdebb52baae949763552feaafc3
LuisaGa96 <luisagalazmp@gmail.com>
2025-03-25 17:55:22 -0700

    Se tiene la info basica para el comienzo del proyecto

---
+++ clase_3/proyecto/js/arregloObjetosMuestra.js
@@ -0,0 +1,9 @@
+//arreglo de objetos con al menos 5 productos, cada uno con las propiedades nombre, precio y categoría.
+
+export const productos = [
+    { nombre: "Camiseta", precio: 15, categoria: "Ropa", img:'' },
+    { nombre: "Laptop", precio: 800, categoria: "Electrónica", img:'' },
+    { nombre: "Libro", precio: 12, categoria: "Educación", img:'' },
+    { nombre: "Zapatos", precio: 50, categoria: "Ropa", img:'' },
+    { nombre: "Celular", precio: 600, categoria: "Electrónica", img:'' },
+];
