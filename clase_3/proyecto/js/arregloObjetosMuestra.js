449782e434babedf0d1e9e24c09f051d7a3c340e
LuisaGa96 <luisagalazmp@gmail.com>
2025-03-25 17:56:13 -0700

    se agrega la url de las imagenes

--- clase_3/proyecto/js/arregloObjetosMuestra.js
+++ clase_3/proyecto/js/arregloObjetosMuestra.js
@@ -1,9 +1,9 @@
 //arreglo de objetos con al menos 5 productos, cada uno con las propiedades nombre, precio y categoría.
 
 export const productos = [
-    { nombre: "Camiseta", precio: 15, categoria: "Ropa", img:'' },
-    { nombre: "Laptop", precio: 800, categoria: "Electrónica", img:'' },
-    { nombre: "Libro", precio: 12, categoria: "Educación", img:'' },
-    { nombre: "Zapatos", precio: 50, categoria: "Ropa", img:'' },
-    { nombre: "Celular", precio: 600, categoria: "Electrónica", img:'' },
+    { nombre: "Camiseta", precio: 15, categoria: "Ropa", img:'img/camiseta.avif' },
+    { nombre: "Laptop", precio: 800, categoria: "Electrónica", img:'img/laptop.png' },
+    { nombre: "Libro", precio: 12, categoria: "Educación", img:'img/libro.jpg' },
+    { nombre: "Zapatos", precio: 50, categoria: "Ropa", img:'img/telefono.png' },
+    { nombre: "Celular", precio: 600, categoria: "Electrónica", img:'img/zapatos.webp' },
 ];
